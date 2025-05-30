3a37191fe8ce3232b9c1873e05de8a9d7cac1004
pankkor <pankkor@gmail.com>
2025-05-30 23:39:22 +0200

    embed Info.plist

--- build.sh
+++ build.sh
@@ -15,12 +15,37 @@ for src in src/*.c; do
 
   out="build/$basename_wo_ext"
 
+  # Embedding Info.plist
+  bundle_id="com.pankkor.$basename_wo_ext"
+  bundle_name="$basename_wo_ext"
+  info_plist="./build/${basename_wo_ext}_Info.plist"
+  embed_info_plist_flags="-Wl,-sectcreate,__TEXT,__info_plist,$info_plist"
+
+  cat > build/${basename_wo_ext}_Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>CFBundleIdentifier</key>
+  <string>${bundle_id}</string>
+  <key>CFBundleName</key>
+  <string>${bundle_name}</string>
+  <key>CFBundleVersion</key>
+  <string>1.0</string>
+  <key>MetalCaptureEnabled</key>
+  <true/>
+</dict>
+EOF
+
+  [ $? = 0 ] || die "Failed to write '$info_plist'"
+
   # Build macOS aarch64 OpenGL
-  build_cmd="clang -o $out $src $cc_flags"
+  build_cmd="clang -o $out $src $embed_info_plist_flags $cc_flags"
 
   echo "Building '$src' -> '$out'"
   echo "$build_cmd"
   echo ""
 
   $build_cmd || die "failed to build '$src'!"
+
 done
