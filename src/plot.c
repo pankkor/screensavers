820a5aedd46e1b0a968b1c22d6833cf7709a9123
pankkor <pankkor@gmail.com>
2025-12-16 20:34:45 +0100

    one plot with 3 segmented GL_LINE_STRIP

--- src/plot.c
+++ src/plot.c
@@ -5,7 +5,7 @@
 // Build
 //   ./build.sh
 // Run
-//   ./build/log
+//   ./build/plot
 
 #include "common.h"
 
@@ -14,35 +14,42 @@
 // --------------------------------------
 static const char * const s_plot_vert_src = "                                  \
 #version 410 core                                                              \
-layout(location = 0) in vec2 v_point;                                          \
+layout(location = 0) in float v_point;                                         \
                                                                                \
-out float v_id;                                                                \
+uniform int  u_offset;                                                         \
+uniform uint u_points_size_minus_one; /* power of 2 */                         \
+                                                                               \
+out vec3 col;                                                                  \
                                                                                \
 void main() {                                                                  \
-  v_id = gl_VertexID;                                                          \
-  gl_Position = vec4(2.0 * v_point.x - 1.0, v_point.y * 2.0 - 1.0, 0.0, 1.0);  \
+  uint v_id = (uint(gl_VertexID) + u_offset) & u_points_size_minus_one;        \
+  float dx = 2.0 / u_points_size_minus_one;                                    \
+  float x = -1.0 + dx * v_id;                                                  \
+  gl_Position = vec4(x, v_point * 2.0 - 1.0, 0.0, 1.0);                        \
 }                                                                              \
 ";
 
 static const char * const s_plot_frag_src = "                                  \
 #version 410 core                                                              \
                                                                                \
-in float v_id;                                                                 \
+uniform vec3 u_col;                                                            \
+                                                                               \
+in vec3 col;                                                                   \
 out vec4 frag_col;                                                             \
                                                                                \
 void main() {                                                                  \
-  frag_col = vec4(0.0, v_id / 1024, 0.0 , 1.0);                                \
+  frag_col = vec4(u_col , 1.0);                                                \
 }                                                                              \
 ";
 
 enum {
-  PLOT_POINTS_COUNT = 1024,
+  PLOT_POINTS_COUNT = 1024, // Power of 2
 };
 
 struct plot {
   // Ring buffer of points
-  f32 points[2 * PLOT_POINTS_COUNT]; // 0 (x-axis) - time; 1 (y-axis) - value
-  u32 end;  // last index in points
+  f32 points[PLOT_POINTS_COUNT]; // time
+  u32 end;  // one after last index in points
 };
 
 void start(void) {
@@ -70,29 +77,32 @@ void start(void) {
     s_plot_frag_src
   );
 
-  f32 t_curve0 = 0.0f;
   struct plot plot0 = {0};
-
-  for (i32 i = 0; i < PLOT_POINTS_COUNT; ++i) {
-    plot0.points[i * 2 + 0] = (f32)i / (PLOT_POINTS_COUNT - 1);
-    plot0.points[i * 2 + 1] = sinf32((f32)i / (PLOT_POINTS_COUNT - 1));
-  }
 
   GLuint vao;
   GLuint points_bo;
+  GLuint points_ebo;
   glGenVertexArrays(1, &vao);
   glGenBuffers(1, &points_bo);
+  glGenBuffers(1, &points_ebo);
+  GLint plot_loc_col         = glGetUniformLocation(plot_prog, "u_col");
+  GLint plot_loc_offset      = glGetUniformLocation(plot_prog, "u_offset");
+  GLint plot_loc_points_size_minus_one =
+    glGetUniformLocation(plot_prog, "u_points_size_minus_one");
 
   glUseProgram(plot_prog);
-  glBindVertexArray(vao);
 
+  glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, points_bo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(plot0.points), plot0.points,
       GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-  // TODO: instancing
-  // glVertexAttribDivisor(1, 1);
+  glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, points_ebo);
+  GLuint indices[] = { PLOT_POINTS_COUNT - 1, 0 };
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+      GL_STATIC_DRAW);
 
   // Game loop
   f32 cpu_timer_freq  = read_cpu_timer_freq();
@@ -124,6 +134,20 @@ void start(void) {
     loop_s          += dt;
     loop_count      += 1;
 
+#if 1 // Print average tick time (print could block io)
+    if (tsc > print_dt_tsc) {
+      print_avg_dt_fps(loop_s / loop_count);
+
+      loop_s        = 0.0f;
+      loop_count    = 0;
+      print_dt_tsc  = tsc + 5.0f * cpu_timer_freq;
+    }
+#else
+    (void)loop_s;
+    (void)loop_count;
+    (void)print_dt_tsc;
+#endif
+
 #if 1 // Stop at current frame. Advance 1 frame on Space press
     b32 is_space_up = keycode_is_up(KC_SPACE, &old_kcs, &kcs);
     b32 is_right_up = keycode_is_up(KC_RIGHT, &old_kcs, &kcs);
@@ -139,39 +163,48 @@ void start(void) {
     }
 #endif
 
-#if 1 // Print average tick time (print could block io)
-    if (tsc > print_dt_tsc) {
-      print_avg_dt_fps(loop_s / loop_count);
-
-      loop_s        = 0.0f;
-      loop_count    = 0;
-      print_dt_tsc  = tsc + 5.0f * cpu_timer_freq;
-    }
-#else
-    (void)loop_s;
-    (void)loop_count;
-    (void)print_dt_tsc;
-#endif
+    glUniform1ui(plot_loc_points_size_minus_one, PLOT_POINTS_COUNT - 1);
 
     // Update plots
-    t_curve0 = fmodf32(t_curve0 + dt, 1.0f);
-
+    u32 inserted_idx = plot0.end;
+    plot0.points[plot0.end] = clampf32(dt * 50.0f, 0.0f, 1.0f);
     plot0.end = (plot0.end + 1) % PLOT_POINTS_COUNT;
-    plot0.points[plot0.end * 2 + 0] = (f32)plot0.end / (PLOT_POINTS_COUNT - 1);
-    plot0.points[plot0.end * 2 + 1] = clampf32(dt * 100.0f, 0.0f, 1.0f);
+
+    // Draw plots
+    glBindBuffer(GL_ARRAY_BUFFER, points_bo);
+    i32 subdata_size = sizeof(plot0.points[0]);
+    glBufferSubData(GL_ARRAY_BUFFER, inserted_idx * subdata_size, subdata_size,
+        &plot0.points[inserted_idx]);
 
     // Draw
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.8f, 0.8f, 0.8f, 0.8f);
 
-    // Draw plots
-    glBindBuffer(GL_ARRAY_BUFFER, points_bo);
-    // TODO, only update 1 point
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(plot0.points), plot0.points);
-    glDrawArrays(GL_LINE_STRIP, 0, PLOT_POINTS_COUNT);
+    // Issue 3 draw calls, for 2 segments and 1 for connection in between
+    u64 size0 = PLOT_POINTS_COUNT - plot0.end;
+    u64 size1 = plot0.end;
 
-    // glDrawArrays(GL_LINE_STRIP, 0, plot0.end);
-    // glDrawArraysInstanced(GL_LINE_STRIP, 0, 2, PLOT_POINTS_COUNT);
+    glUniform1i(plot_loc_offset, size0);
+
+    // First segment
+    glUniform3f(plot_loc_col, 1.0f, 0.0f, 0.5f);
+    glDrawArrays(GL_LINE_STRIP, plot0.end, size0);
+
+    // Connect 2 segments
+    if (size1 > 0) {
+      glDrawElements(GL_LINES, 2, GL_UNSIGNED_INT, 0);
+    }
+
+    // Second segment
+    if (size1 > 1) {
+      glDrawArrays(GL_LINE_STRIP, 0, size1);
+    }
+
+#if 1
+    glPointSize(2.0f);
+    glUniform3f(plot_loc_col, 0.2f, 0.2f, 0.2f);
+    glDrawArrays(GL_POINTS, 0, PLOT_POINTS_COUNT);
+#endif
 
     window_flush(&w);
   }
